2ba92dd444a0e251e5ec290c334405148c5e66ff
sammy310 <kipper098@gmail.com>
2021-02-23 00:34:03 +0900

    Trian - 리더보드 추가, Firebase의 Firestore, Analytics 추가

---
+++ game/Trian/firebase.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDqCc2p0HfKQ8hZeeftvy_BAImqOpDKD1U",
+    authDomain: "trian-49247795.firebaseapp.com",
+    databaseURL: "https://trian-49247795.firebaseio.com",
+    projectId: "trian-49247795",
+    storageBucket: "trian-49247795.appspot.com",
+    messagingSenderId: "119385446960",
+    appId: "1:119385446960:web:9ddf2dbcde8d35bc412bf5",
+    measurementId: "G-LGCCYVRELZ"
+};
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+const analytics = firebase.analytics();
