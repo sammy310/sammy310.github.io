1e180683b30a71389f256519b0950ada46b7a9c3
sammy310 <kipper098@gmail.com>
2022-02-18 17:55:30 +0900

    The Barricade 추가

--- dev/old/init_html.js
+++ dev/old/init_html.js
@@ -54,6 +54,7 @@ $(document).ready(function () {
         </li>\
         <li id="nav_develop"><a href="/dev/old/develop/develop.html">계발</a>\
             <ul id="nav_develop_list" class="nav_child_first">\
+                <li><a href="/dev/old/develop/barricade/barricade.html">바리케이드</a></li>\
                 <li><a href="/dev/old/develop/minus/minus.html">마이너스</a></li>\
                 <li><a href="/dev/old/develop/trian/trian.html">Trian</a></li>\
                 <li><a href="/dev/old/develop/eight_queens_problem/eight_queens_problem.html">8 퀸 문제</a></li>\
